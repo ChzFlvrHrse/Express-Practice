5beaba24878363483e503830ad82f40699c219be
Nathan <nscott1010@gmail.com>
2023-06-22 16:22:21 -0500

    middleware error handlers done phase 1-2 done

--- backend/app.js
+++ backend/app.js
@@ -7,6 +7,8 @@ const helmet = require('helmet');
 const cookieParser = require('cookie-parser');
 const routes = require('./routes');
 
+const { ValidationError } = require('sequelize')
+
 const { environment } = require('./config');
 const isProduction = environment === 'production';
 
@@ -35,6 +37,33 @@ app.use(
         }
     })
 )
+
+app.use((_req, _res, next) => {
+    const err = new Error("The requested resource couldn't be found.");
+    err.title = "Resource Not Found";
+    err.errors = ["The requested resource couldn't be found."];
+    err.status = 404;
+    next(err);
+});
+
+app.use((err, _req, _res, next) => {
+    if (err instanceof ValidationError) {
+        err.errors = err.errors.map(e => e.message);
+        err.title = 'Validation error';
+    }
+    next(err);
+})
+
+app.use((err, _req, res, _next) => {
+    res.status(err.status || 500);
+    console.error(err);
+    res.json({
+        title: err.title || 'Server Error',
+        message: err.message,
+        errors: err.error,
+        stack: isProduction ? null : err.stack
+    });
+});
 
 app.use(routes);
 
