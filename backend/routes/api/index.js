29badab26e83e4644264c1579b35ea307e9f66ff
Nathan <nscott1010@gmail.com>
2023-07-14 14:33:43 -0500

    user session login working

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,8 +1,16 @@
 const router = require('express').Router();
+const sessionRouter = require('./session')
+const userRouter = require('./users')
 const { restoreUser } = require('../../utils/auth.js');
 
 router.use(restoreUser);
 
+router.use('/session', sessionRouter);
 
+router.use('/user', userRouter);
+
+router.post('/test', (req, res) => {
+    res.json({ requestBody: req.body });
+});
 
 module.exports  = router;
