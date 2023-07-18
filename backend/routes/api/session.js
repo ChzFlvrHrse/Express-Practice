e7596ed30dea9faf03cf47774ab3efec627e7af0
Nathan <nscott1010@gmail.com>
2023-07-17 22:57:04 -0500

    login credential check

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const { check } = require('express-validator');
+const { handleValidationErrors } = require('../../utils/validation');
 
 const { setTokenCookie, restoreUser } = require('../../utils/auth');
 const { User } = require('../../db/models');
@@ -24,7 +26,7 @@ router.post('/', async (req, res, next) => {
 
 router.delete('/', async (_req, res) => {
     res.clearCookie('token');
-    return res.json({ message: "Log out successful"});
+    return res.json({ message: "Log out successful" });
 });
 
 router.get('/', restoreUser, (req, res) => {
@@ -36,5 +38,37 @@ router.get('/', restoreUser, (req, res) => {
     }
     return res.json({})
 });
+
+const validateLogin = [
+    check('credential')
+        .exists({ checkFalsy: true })
+        .notEmpty()
+        .withMessage('Please provide a valid email or username.'),
+    check('password')
+        .exists({ checkFalsy: true })
+        .withMessage('Please provide a password.'),
+    handleValidationErrors
+];
+
+router.post('/', validateLogin, async (req, res, next) => {
+        const { credential, password } = req.body;
+
+        const user = await User.login({ credential, password });
+
+        if (!user) {
+            const err = new Error('Login failed');
+            err.status = 401;
+            err.title = 'Login failed';
+            err.errors = ['The provided credentials were invalid.'];
+            return next(err);
+        }
+
+        await setTokenCookie(res, user);
+
+        return res.json({
+            user
+        });
+    }
+);
 
 module.exports = router;
