7762ff8d72ee4ee7d669b560d61aeb027cc3626c
Nathan <nscott1010@gmail.com>
2023-08-15 17:08:15 -0500

    more seed data

--- backend/db/seeders/20230814220116-accessory.js
+++ backend/db/seeders/20230814220116-accessory.js
@@ -311,18 +311,18 @@ module.exports = {
       {
         name: 'Model S',
         area: 'Parts',
-        title: '',
-        price: ,
-        img: '',
-        img2: ''
+        title: 'Model S Wiper Blade',
+        price: 28,
+        img: 'https://digitalassets-shop.tesla.com/image/upload/f_auto,q_auto/v1/content/dam/tesla/studio/CAR_ACCESSORIES/MODEL_S/EXTERIOR/1562215-00-A_0_2000.jpg',
+        img2: 'https://digitalassets-shop.tesla.com/image/upload/f_auto,q_auto/v1/content/dam/tesla/CAR_ACCESSORIES/MODEL_S/EXTERIOR/1562215-00-A_1_2000.jpg'
       },
       {
         name: 'Model S',
         area: 'Parts',
-        title: '',
-        price: ,
-        img: '',
-        img2: ''
+        title: 'Model S/X Air Filter',
+        price: 34,
+        img: 'https://digitalassets-shop.tesla.com/image/upload/f_auto,q_auto/v1/content/dam/tesla/studio/CAR_ACCESSORIES/MODEL_S/INTERIOR/1688910-00-C_0_2000.jpg',
+        img2: 'https://digitalassets-shop.tesla.com/image/upload/f_auto,q_auto/v1/content/dam/tesla/CAR_ACCESSORIES/MODEL_S/INTERIOR/1688910-00-C_1_2000.jpg'
       }
     ])
   },
