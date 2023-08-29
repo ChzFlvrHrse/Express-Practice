01726af6b9aa1df8c82d67f3b7423b6f060936aa
Nathan <nscott1010@gmail.com>
2023-08-29 15:19:08 -0500

    store in development

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,20 @@
-
+import React, { useState, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Route, Switch } from 'react-router-dom';
+import * as sessionActions from './store/session';
 
 function App() {
+  const dispatch = useDispatch();
+  const [isLodaded, setIsLoaded] = useState(false);
+
+  useEffect(() => {
+    dispatch(sessionActions.restoreUser()).then(() => setIsLoaded(true));
+  }, [dispatch]);
+
   return (
-    <h1>Hello from App</h1>
+    <>
+      <h1>Welcome to the App</h1>
+    </>
   );
 }
 
